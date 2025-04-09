021610690c874f1fa461d3a4a0caae2fe8381711
Andres <andresveca2637@gmail.com>
2025-04-09 18:47:45 -0500

    Sucursales Front

--- resources/views/sucursale/index.blade.php
+++ resources/views/sucursale/index.blade.php
@@ -16,11 +16,12 @@
                                 {{ __('Sucursales') }}
                             </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('sucursales.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                            <div class="float-right">
+                                <a href="{{ route('sucursales.create') }}" class="btn btn-primary btn-sm float-right"
+                                    data-placement="left">
+                                    {{ __('Create New') }}
                                 </a>
-                              </div>
+                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -35,12 +36,10 @@
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
-                                        
-									<th >Nombre</th>
-									<th >Direccion</th>
-									<th >Telefono</th>
-									<th >User Id</th>
 
+                                        <th>Nombre</th>
+                                        <th>Direccion</th>
+                                        <th>Telefono</th>
                                         <th></th>
                                     </tr>
                                 </thead>
@@ -48,19 +47,24 @@
                                     @foreach ($sucursales as $sucursale)
                                         <tr>
                                             <td>{{ ++$i }}</td>
-                                            
-										<td >{{ $sucursale->nombre }}</td>
-										<td >{{ $sucursale->direccion }}</td>
-										<td >{{ $sucursale->telefono }}</td>
-										<td >{{ $sucursale->user_id }}</td>
 
+                                            <td>{{ $sucursale->nombre }}</td>
+                                            <td>{{ $sucursale->direccion }}</td>
+                                            <td>{{ $sucursale->telefono }}</td>
+    
                                             <td>
                                                 <form action="{{ route('sucursales.destroy', $sucursale->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('sucursales.show', $sucursale->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('sucursales.edit', $sucursale->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('sucursales.show', $sucursale->id) }}"><i
+                                                            class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('sucursales.edit', $sucursale->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"
+                                                        onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i
+                                                            class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
