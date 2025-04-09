2d3110b6c70e2294edddd8e596b5f60cc4db67f7
Andres <andresveca2637@gmail.com>
2025-04-09 00:42:11 -0500

    Nav Sucursales

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,13 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('sucursales.index')" :active="request()->routeIs('sucursales.index')">
+                        {{ __('Sucursales') }}
+                    </x-nav-link>
+                </div>
             </div>
+         
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
