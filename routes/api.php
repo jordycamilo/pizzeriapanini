61e9060c9c57fc51a57675a7f5be729b0d7fdacf
jordy diaz <jordycamilodiazladino@gmail.com>
2025-05-27 23:31:00 -0500

    api clientes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\api\SucursaleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\ClientController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -15,7 +16,14 @@ Route::post('/sucursales',[SucursaleController::class,'store'])->name('sucursale
 Route::put('/sucursales/{id}',[SucursaleController::class,'update'])->name('sucursales.update');
 Route::delete('/sucursales/{id}',[SucursaleController::class,'destroy'])->name('sucursales.destroy');
 
-Route::get('/users', [UserController::class, 'index'])->name('users');
+//Route::get('/users', [UserController::class, 'index'])->name('users');
 
-//Route::resource('sucursales', SucursaleController::class);
+Route::get('/clients', [ClientController::class, 'index'])->name('clients');
+Route::get('/clients/{id}',[ClientController::class,'show'])->name('clients.show');
+Route::post('/clients',[ClientController::class,'store'])->name('clients');
+Route::put('/clients/{id}',[ClientController::class,'update'])->name('clients.update');
+Route::delete('/clients/{id}',[ClientController::class,'destroy'])->name('clients.destroy');
+
+
+//Route::resource('clients', SucursaleController::class);
 //Route::resource('users', UserController::class);
